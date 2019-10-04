2e7f59037d03fbf5d7372de9bdc699588d6cd19f
Thelsandro <39775561+thelsandroantunes@users.noreply.github.com>
2019-10-03 21:34:42 -0400

    frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -14,7 +17,9 @@ useUnifiedTopology: true,
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição e delete)
 // 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
